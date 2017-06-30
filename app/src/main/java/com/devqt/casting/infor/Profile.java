5c54c7feb22a427cd662cb865d9a3246ef10a079
BelRoy <oksiwhite@ukr.net>
2017-06-30 15:44:27 +0300

    new icon, add json file
    new icon, add json file

--- app/src/main/java/com/devqt/casting/infor/Profile.java
+++ app/src/main/java/com/devqt/casting/infor/Profile.java
@@ -1,9 +1,17 @@
 package com.devqt.casting.infor;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by supervisor on 29.06.2017.
- */
+
 public class Profile extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedStateInstance){
+
+        super.onCreate(savedStateInstance);
+        setContentView(R.layout.profile_act);
+
+    }
+
 }
