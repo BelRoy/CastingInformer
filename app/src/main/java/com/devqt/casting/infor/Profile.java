01a35c3112aa58b267d5c4feb77835d6cb104648
BelRoy <oksiwhite@ukr.net>
2017-08-16 16:07:48 +0300

    Fix manifest

--- app/src/main/java/com/devqt/casting/infor/Profile.java
+++ app/src/main/java/com/devqt/casting/infor/Profile.java
@@ -3,10 +3,13 @@ package com.devqt.casting.infor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class Profile extends AppCompatActivity {
 
+    String[] sex = {"Male", "Female"};
 
     protected void onCreate(Bundle savedStateInstance){
 
@@ -17,6 +20,13 @@ public class Profile extends AppCompatActivity {
       //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
      //   setSupportActionBar(toolbar);
 
+        Spinner spinner = (Spinner) findViewById(R.id.sex);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sex);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
 
     }
     @Override
